dccdc2f9f8d2d026204401f05f94c2bc0683d311
Zoe Holubkova <zzoehol@gmail.com>
2021-06-20 08:46:12 +0300

    Add possibility to player O to make moves

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,6 @@
-print('Enter cells: ')
-cells = input()
-
-x = cells.count('X')
-o = cells.count('O')
-pl_x = 'X'
-pl_o = 'O'
+PL_X = 'X'
+PL_O = 'O'
+cells = '_________'
 
 
 def print_cells(cell):
@@ -43,49 +39,61 @@ def check_winner(inp_data, player):
     return 0  # no win
 
 
+def enter_coordinates(deck, player):
+    print('Make the move!')
+
+    check = 0
+    while check == 0:
+        print('Enter the coordinates: ')
+        i, j = input().split()
+        try:
+            int(i)
+            int(j)
+        except ValueError:
+            print('You should enter numbers!')
+            continue
+        else:
+            if int(i) > 3 or int(j) > 3:
+                print('Coordinates should be from 1 to 3!')
+                continue
+
+            index = (int(i) - 1) * 3 + int(j) - 1
+            if deck[index] != '_':
+                print('This cell is occupied! Choose another one!')
+                continue
+
+            deck = deck[:index] + player + deck[index + 1:]
+            check = 1
+            return deck
+
+
 print_cells(cells)
-
-check_win_x = check_winner(cells, pl_x)
-check_win_o = check_winner(cells, pl_o)
-
-print(check_win_x)
-print(check_win_o)
-
-if abs(x - o) >= 2:
-    print('Impossible')
-elif check_win_x == 1 and check_win_o == 1:
-    print('Impossible')
-elif check_win_x == 1:
-    print('X wins')
-elif check_win_o == 1:
-    print('O wins')
-elif cells.count('_') == 0:
-    print('Draw')
-else:
-    print('Game not finished')
-
-print('Make the move!')
-
-check = 0
-while check == 0:
-    print('Enter the coordinates: ')
-    i, j = input().split()
-    try:
-        int(i)
-        int(j)
-    except ValueError:
-        print('You should enter numbers!')
-        continue
+move = 0
+while move < 9:
+    if move % 2 == 0:
+        cells = enter_coordinates(cells, PL_X)
     else:
-        if int(i) > 3 or int(j) > 3:
-            print('Coordinates should be from 1 to 3!')
-            continue
+        cells = enter_coordinates(cells, PL_O)
 
-        index = (int(i) - 1) * 3 + int(j) - 1
-        if cells[index] != '_':
-            print('This cell is occupied! Choose another one!')
-            continue
+    print_cells(cells)
 
-        cells = cells[:index] + 'X' + cells[index + 1:]
-        print_cells(cells)
-        check = 1
+    x = cells.count(PL_X)
+    o = cells.count(PL_O)
+
+    check_win_x = check_winner(cells, PL_X)
+    check_win_o = check_winner(cells, PL_O)
+
+    if abs(x - o) >= 2:
+        print('Impossible')
+    elif check_win_x == 1 and check_win_o == 1:
+        print('Impossible')
+    elif check_win_x == 1:
+        print('X wins')
+        break
+    elif check_win_o == 1:
+        print('O wins')
+        break
+    elif cells.count('_') == 0:
+        print('Draw')
+
+    move += 1
